0d423586b3e3bf1a66f115b402d786d0eb226085
sudaisabubakar22-code <sudaisabubakar22@gmail.com>
2026-03-02 11:44:10 +0100

    Create Main.java as the main entry point

---
+++ src/main/java/com/weapontrims/Main.java
@@ -0,0 +1,8 @@
+package com.weapontrims;
+
+public class Main extends WeaponTrimPlugin {
+    public static void main(String[] args) {
+        // Main entry point logic here
+        System.out.println("Welcome to the Domain Weapons Plugin!");
+    }
+}
